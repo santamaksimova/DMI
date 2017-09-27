72cedaaa3bb04a801364a7ad7ce1a39d133cff09
Guest <santamaksimova11@inbox.lv>
2017-09-27 11:49:01 +0300

    20170927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+#4. piemers
+echo "tiek pildīts skripts:" $0
+echo $0
+echo $n
+echo "Skriptam nodoto argumentu skaits:"$#
+echo "Skriptam nodoti argumenti (attēlošana/grupēšana 1):"$*
+echo "Skriptam nodoti argumenti (attēlošana/grupēšana 2):"$@
+#echo $?
+echo "Skriptam piešķirtais procesa numurs"$$
+echo $!
 
-NAME="Vaards Uzvaards"
+#3. piemērs
+#NAME="Vaards Uzvaards"
+#echo $NAME
+#unset NAME
 #echo $NAME
 
+#2. piemērs
+#NAME="Vaards Uzvaards"
 #readonly NAME
 #echo $NAME
 #NAME="Uzvaards Vaards"
 #echo $NAME
 
-echo $NAME
-unset NAME
-echo $NAME
+#1. piemērs
+#NAME="Vaards Uzvaards"
+#echo $NAME
