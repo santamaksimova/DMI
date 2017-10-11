4b7398dddf9b5324326e8cecb95a82b80d8583f9
Guest <santamaksimova11@inbox.lv>
2017-10-11 11:51:32 +0300

    20171011_11_50

--- shell-operators.sh
+++ shell-operators.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+# 4. piemērs - operācijas ar argumentiem
+if [ $# == 2 ]
+# ja (if) skriptam nodoto argumentu skaits ($#) ir vienāds (==) ar 2
+then
+# tad (then)izpildam sekojošās darbības
+a=$1
+b=$2
+val41=`expr $a + $b`
+echo "$a + $b = "$val41
+val42=`expr $a - $b`
+echo "$a - $b = "$val42
+val43=`expr $a \* $b`
+echo "$a * $b = "$val43
+val44=`expr $a / $b`
+echo "$a / $b = "$val44
+val45=`expr $a % $b`
+echo "$a % $b = "$val45
+#....līdz šai vietai
+fi
+
 # 3. piemērs - operācijas ar mainīgajiem
+#: <<'END'
 a=29
 b=15
 val31=`expr $a + $b`
@@ -13,6 +34,7 @@ val34=`expr $a / $b`
 echo "$a / $b = "$val34
 val35=`expr $a % $b`
 echo "$a % $b = "$val35
+#END
 
 #2. piemērs - operācijas ar konstantēm
 : <<'END'
